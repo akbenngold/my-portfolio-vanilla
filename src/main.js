4813c4541046d36203e8a3596438046aa358cc3b
Ojima <akbenngold@gmail.com>
2023-12-01 18:22:11 +0100

    porgtfolio

--- src/main.js
+++ src/main.js
@@ -3,4 +3,5 @@ import "../styles/style.css";
 import "../styles/components/header.css";
 import "../styles/components/hero.css";
 import "../styles/components/about.css";
+import "../styles/components/portfolio.css";
 import "../styles/utility.css";
