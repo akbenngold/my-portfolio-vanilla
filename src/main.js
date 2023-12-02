355e23cd9e6b79f3898678191569a8dafc4faa76
Ojima <akbenngold@gmail.com>
2023-12-02 09:55:07 +0100

    static

--- src/main.js
+++ src/main.js
@@ -4,4 +4,6 @@ import "../styles/components/header.css";
 import "../styles/components/hero.css";
 import "../styles/components/about.css";
 import "../styles/components/portfolio.css";
+import "../styles/components/contact.css";
+import "../styles/components/footer.css";
 import "../styles/utility.css";
